050878041f4436f9785fff1f35adbef312265863
Susan Grg <56074470+Warcier@users.noreply.github.com>
2023-01-08 21:39:27 +0800

    Navbar component created
    Co-authored-by: Mahnoor0shaikh <Mahnoor0shaikh@users.noreply.github.com>
    Co-authored-by: Luqman679 <Luqman679@users.noreply.github.com>

--- src/renderer/components/navbar.tsx
+++ src/renderer/components/navbar.tsx
@@ -1,7 +1,91 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faWallet } from '@fortawesome/free-solid-svg-icons';
 
-const navbar = () => {
-  return <div>navbar</div>;
+const Navbar = () => {
+  return (
+    <div>
+      <div className="flex">
+        <div className="flex flex-col h-screen p-3 bg-white shadow w-60">
+          <div className="space-y-3">
+            <div className="flex items-center">
+              <h2 className="text-xl font-bold">Dashboard</h2>
+            </div>
+            <div className="flex-1">
+              <ul className="pt-2 pb-4 space-y-1 text-sm">
+                <li className="rounded-sm">
+                  <a
+                    href="#"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <FontAwesomeIcon icon={faWallet} />
+                    <div>
+                      <Link to="/test" className="btn">
+                        Wallet
+                        <br />
+                      </Link>
+                    </div>
+                  </a>
+                </li>
+                <li className="rounded-sm">
+                  <a
+                    href="#"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+
+                    <span>Inbox</span>
+                  </a>
+                </li>
+                <li className="rounded-sm">
+                  <a
+                    href="#"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <div>
+                      <Link to="/family" className="btn">
+                        family
+                      </Link>
+                    </div>
+                  </a>
+                </li>
+                <li className="rounded-sm">
+                  <a
+                    href="#"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <span>Settings</span>
+                  </a>
+                </li>
+                <li className="rounded-sm">
+                  <a
+                    href="#"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                      />
+                    </svg>
+                    <span>Logout</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default navbar;
+export default Navbar;
