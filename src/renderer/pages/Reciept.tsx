ed3f16d7d718c7e3477550de84d0eb7904b6bc61
KenYeung <kyeunga25@gmail.com>
2023-01-15 00:27:26 +0800

    making the page now can read images from Firebase Storage and show them after refreshing the page

--- src/renderer/pages/Reciept.tsx
+++ src/renderer/pages/Reciept.tsx
@@ -1,19 +1,32 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { storage } from 'renderer/firebase';
-import { ref, uploadBytes } from 'firebase/storage';
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
 import { v4 } from 'uuid';
 
 const Receipts = () => {
   const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, "images/");
+
   const uploadImage = () => {
     if (imageUpload == null) return;
 
     const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
     uploadBytes(imageRef, imageUpload).then(() => {
-      alert("Image Uplaoded");
+      alert('Image Uplaoded');
     });
   };
+
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        })
+      })
+    })
+  }, []);
 
   return (
     <div>
@@ -28,6 +41,17 @@ const Receipts = () => {
         <button type="submit" onClick={uploadImage} className="btn">
           Upload
         </button>
+      </div>
+      <div className="flex items-center justify-center">
+        {imageList.map((url) => {
+          return (
+            <img
+              src={url}
+              className="image"
+              alt=""
+            />
+          );
+        })}
       </div>
       <div className="flex items-center justify-center">
         <Link to="/" className="btn">
