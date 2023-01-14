1244ef272743a7a0b8e53334b4424db39e612f28
KenYeung <kyeunga25@gmail.com>
2023-01-14 23:58:24 +0800

    Adding a upload module to upload file to firebase db2

--- src/renderer/pages/Reciept.tsx
+++ src/renderer/pages/Reciept.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { storage } from 'renderer/firebase';
+import { ref, uploadBytes } from 'firebase/storage';
+import { v4 } from 'uuid';
 
 const Receipts = () => {
+  const [imageUpload, setImageUpload] = useState(null);
+  const uploadImage = () => {
+    if (imageUpload == null) return;
+
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert("Image Uplaoded");
+    });
+  };
+
   return (
     <div>
       <div className="flex items-center justify-center">
         <h1>Upload your invoice/receipt:</h1>
-        <input type="file" />
+        <input
+          type="file"
+          onChange={(event) => {
+            setImageUpload(event.target.files[0]);
+          }}
+        />
+        <button type="submit" onClick={uploadImage} className="btn">
+          Upload
+        </button>
       </div>
       <div className="flex items-center justify-center">
         <Link to="/" className="btn">
