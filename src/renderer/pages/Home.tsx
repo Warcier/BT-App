c9d761064e2d0a8df2d18983104b45bcd6968318
Susan Grg <56074470+Warcier@users.noreply.github.com>
2023-01-30 15:22:34 +0800

    minor changes on Card Table

--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -5,14 +5,14 @@ import SavingCard from '../components/SavingCard';
 import CCardModal from '../components/modal/CCardModal';
 import CCard from '../components/CCard';
 import SpendingGraph from '../components/graph/SpendingGraph';
+import ExpenseForm from '../components/forms/ExpenseForm';
 
 function Home() {
   return (
     <>
       <div className="h-screen flex items-center justify-center space-x-2">
         <h1 className="text-EerieBlack-500 ">
-          Hello Tailwind
-          <br />
+          <ExpenseForm />
         </h1>
       </div>
     </>
