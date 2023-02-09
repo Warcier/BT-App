51287294e9397634284a9acbc4f2ba2237069d0d
Luqman679 <luqman.ahmed679@gmail.com>
2023-02-09 10:44:15 +0800

    just made some small changes to the home page

--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import TextTag from 'renderer/components/TextTag';
+import BudgetForm from 'renderer/components/forms/BudgetForm';
+import PInfoForm from 'renderer/components/forms/PInfoForm';
 import PInfoModal from '../components/modal/PInfoModal';
 import SetBudgetModal from '../components/modal/SetBudgetModal';
 import SavingCard from '../components/SavingCard';
@@ -9,13 +12,22 @@ import ExpenseForm from '../components/forms/ExpenseForm';
 
 function Home() {
   return (
-    <>
-      <div className="h-screen flex items-center justify-center space-x-2">
+    <div>
+      <div className="h-screen flex space-x-2">
         <h1 className="text-EerieBlack-500 ">
           <ExpenseForm />
+
+          <div className="h-56 grid grid-cols-3 gap-4 content-start px-10 py-10">
+            <div>
+              <SavingCard />
+            </div>
+            <div>
+              <BudgetForm />
+            </div>
+          </div>
         </h1>
       </div>
-    </>
+    </div>
   );
 }
 
