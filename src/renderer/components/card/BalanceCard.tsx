f0ddd833e6d8dde38c02cfcfcb7fb2a6feb62cfd
Luqman679 <luqman.ahmed679@gmail.com>
2023-03-14 16:10:59 +0800

    minor changes

--- src/renderer/components/card/BalanceCard.tsx
+++ src/renderer/components/card/BalanceCard.tsx
@@ -18,23 +18,23 @@ const BalanceCard = () => {
 
   return (
     <>
-      <div className=" w-[24.4rem] h-[14.2rem] stats bg-gradient-to-r from-blue-600 to-blue-400 text-primary-content container items-center justify-center ">
-        <div className="flex flex-col ">
+      <div className=" w-[24.4rem] h-[13.2rem] stats bg-gradient-to-r from-blue-600 to-blue-400 text-primary-content container py-2">
+        <div className="">
           <div className="grow">
-            <div className="font-semibold text-2xl text-white py-3">
+            <div className="font-semibold text-2xl text-white py-3 pl-24">
               Current balance
             </div>
           </div>
           <div>
-            <div className="text-6xl text-white font-bold">${currentBalance}</div>
+            <div className="text-6xl pl-24 text-white font-bold">${currentBalance}</div>
           </div>
           <div>
-            <div className="stat-actions">
+            <div className="stat-actions pl-32">
               <SetBalanceModal />
             </div>
           </div>
+          <div className="px-20" />
         </div>
-        <div className="stat items-center justify-center" />
       </div>
     </>
   );
