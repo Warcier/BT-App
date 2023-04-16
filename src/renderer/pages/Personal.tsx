39b700a635b3164c54eb6dcc5b2fa7f403603b0d
Ken Yeung <kyeunga25@gmail.com>
2023-04-16 22:50:52 +0800

    Fix bug in Personal page

--- src/renderer/pages/Personal.tsx
+++ src/renderer/pages/Personal.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PersonalCard from '../components/card/PersonalCard';
 import CardDetailTable from '../components/table/CardDetailTable';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
+import ImageUpload from './FormPage/ImageUpload';
 
 function Personal() {
   return (
@@ -27,17 +30,18 @@ function Personal() {
         </div>
         <div>
           Address:
-    <>
-      <div>
-        <PersonalCard />
-        <div className=" m-5 flex items-center justify-center bg-white space-x-2">
-          <h1 className="text-blue-500">
-            <h1 className="text-center font-bold text-4xl my-3">Credit Card</h1>
-            <CardDetailTable />
-          </h1>
+        </div>
+        <div>
+          <PersonalCard />
+          <div className=" m-5 flex items-center justify-center bg-white space-x-2">
+            <h1 className="text-blue-500">
+              <h1 className="text-center font-bold text-4xl my-3">Credit Card</h1>
+              <CardDetailTable />
+            </h1>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
