a0537721c29f67bdc1e1365ac0c898a85ff9ab99
Ken Yeung <kyeunga25@gmail.com>
2023-04-16 23:24:41 +0800

    Remove wallet page

--- src/renderer/components/NavBar.tsx
+++ src/renderer/components/NavBar.tsx
@@ -44,15 +44,15 @@ const NavBar = () => {
             </Link>
           </li>
 
-          <li className="hover:bg-blue-600 w-44">
-            <Link
-              to="/family"
-              className="text-xl h-16 px-6 flex flex justify-center items-center w-full space-x-5"
-            >
-              <FontAwesomeIcon icon={faUser} />
-              <p className="font-serif">Profile</p>
-            </Link>
-          </li>
+          {/*<li className="hover:bg-blue-600 w-44">*/}
+          {/*  <Link*/}
+          {/*    to="/family"*/}
+          {/*    className="text-xl h-16 px-6 flex flex justify-center items-center w-full space-x-5"*/}
+          {/*  >*/}
+          {/*    <FontAwesomeIcon icon={faUser} />*/}
+          {/*    <p className="font-serif">Profile</p>*/}
+          {/*  </Link>*/}
+          {/*</li>*/}
 
           <li className="hover:bg-blue-600 w-44">
             <Link
