03ce9c09f475173799970e8ee75a1427e2776fac
Susan Grg <56074470+Warcier@users.noreply.github.com>
2023-04-17 22:51:14 +0800

    create project

--- postcss.config.js
+++
@@ -1,4 +0,0 @@
-/* eslint global-require: off, import/no-extraneous-dependencies: off */
-module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
-};
--- src/renderer/components/function.tsx
+++
@@ -1,6 +0,0 @@
-export function spacesAt4Char(text: string){
-  const newText = text.match(/.{1,4}/g);
-  return newText.join(' ');
-}
-
-export default spacesAt4Char;
