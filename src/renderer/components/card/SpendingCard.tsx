ffb471aa55887f6e4a91db969912a5ecc4b30778
Luqman679 <luqman.ahmed679@gmail.com>
2023-04-19 20:50:21 +0800

    changed icon for reciept

--- src/renderer/components/card/SpendingCard.tsx
+++ src/renderer/components/card/SpendingCard.tsx
@@ -11,7 +11,7 @@ function SpendingCard(props) {
     }
   }, []);
   return (
-    <div className="stats text-white transition delay-75 ease-in-out bg-blue-600 hover:bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 transform active:-translate-y-2 shadow shadow-xl w-[24.4rem] height-fixed hover:shadow-lg">
+    <div className="stats text-white w-[24.4rem] height-fixed transition delay-75 ease-in-out bg-blue-600 hover:bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 transform active:-translate-y-2 shadow shadow-xl hover:shadow-lg">
       <div className="stat">
         <div className=" text-xl pb-1">Spending Limits</div>
         <progress
